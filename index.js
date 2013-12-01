997caae1056adde17f4616b1dcb14d8c91a10930
Brandon Valosek <bvalosek@w2odigital.com>
2013-11-30 18:18:30 -0600

    connection fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var constants = require('./lib/constants.js');
 
 var SC2Ranks = module.exports = {
 
-  Connection: require('./lib/Sc2Ranks.js'),
+  Connection: require('./lib/Connection.js'),
 
   Clan: require('./lib/models/Clan.js'),
   Character: require('./lib/models/Character.js')
